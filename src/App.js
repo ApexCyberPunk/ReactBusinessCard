3538e5a3c06099cd6a22f6eed5de6997c604a07c
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-29 00:31:22 -0700

    lets Git it
    updating

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>First React App</p>
-        <h1>I am learning React</h1>
-      </header>
-    </div>
-  );
+  return
 }
 
 export default App;
