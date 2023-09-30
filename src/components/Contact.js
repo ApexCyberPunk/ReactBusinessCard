64b581753c903dd188cfb0190fbac6f617ab8bb6
snovacain <61154743+snovacain@users.noreply.github.com>
2023-09-29 21:29:43 -0700

    break

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <>
+       <div>
       <h1>Contact:</h1>
-      <p color='gold'><a color='gold' href="mailto:codingndevelopment@gmail.com">Email</a></p>
+    <a href='www.fiverr.com'><img width='100px' alt='fiverr' src={require("./images/fiver.jpg")}></img></a>
     </div>
+    <button href="mailto:codingndevelopment@gmail.com"><a color='gold' href="mailto:codingndevelopment@gmail.com">Email</a></button>
+    </>
+   
   )
 }
 
